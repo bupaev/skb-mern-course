4880bb4fbf3e4d5110cc27cafced7ae127b375b5
Paul Buramensky <bupaev@gmail.com>
2016-11-10 22:50:25 +0600

    Improve code for 2A and 2B

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,7 @@
 
 - Init project
 - Update description and file structure
+
+### [v1.1.0] - 2016-11-10
+
+- Finish tasks for second lesson
--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 function sum(a = 0, b = 0) {
   return (Number(a) || 0) + (Number(b) || 0); // Use || to replace NaN
 }
 
 function getInitials(fullname) {
-  const words = fullname.trim().split(/\s+/);
+
+  // Empty name, digits and special characters is not allowed
+  if (!fullname || /[\d$&+,:;=?@#|/<>.^*()%!_]/i.test(fullname)) {
+    return 'Invalid fullname';
+  }
+
+  const words = fullname
+    .trim()
+    .toLowerCase()
+    .split(/ +/)
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
 
   if (words.length > 3) {
     return 'Invalid fullname';
   }
 
   return words.splice(0, words.length - 1).reduce(function(a, b) {
-    return a + b[0] + '. ';
-  }, words[0] + ' ');
+    return a + ' ' + b[0] + '.';
+  }, words[0]);
 }
 
 app.get('/task2A', (req, res) => {
-  res.send(`Вывод: ${sum(req.query.a, req.query.b)}`);
+  const result = sum(req.query.a, req.query.b).toString();
+  res.send(result);
 });
 
 app.get('/task2B', (req, res) => {
