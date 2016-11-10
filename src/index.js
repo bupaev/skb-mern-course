ad266fb5cc2ae376b5b30c4483de845877ed4514
Paul Buramensky <bupaev@gmail.com>
2016-11-10 19:26:58 +0600

    Task 2B

--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@ function sum(a = 0, b = 0) {
   return (Number(a) || 0) + (Number(b) || 0); // Use || to replace NaN
 }
 
+function getInitials(fullname) {
+  const words = fullname.trim().split(/\s+/);
+
+  if (words.length > 3) {
+    return 'Invalid fullname';
+  }
+
+  return words.splice(0, words.length - 1).reduce(function(a, b) {
+    return a + b[0] + '. ';
+  }, words[0] + ' ');
+}
+
 app.get('/task2A', (req, res) => {
   res.send(`Вывод: ${sum(req.query.a, req.query.b)}`);
+});
+
+app.get('/task2B', (req, res) => {
+  res.send(getInitials(req.query.fullname));
 });
 
 app.listen(3000, () => {
